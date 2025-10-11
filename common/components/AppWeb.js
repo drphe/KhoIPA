b1322fa2a8b9aca77f8f52bc2f1d0f6ced8e3d2c
drphe <138484118+drphe@users.noreply.github.com>
2025-10-11 09:25:22 +0700

    Update AppWeb.js

--- common/components/AppWeb.js
+++ common/components/AppWeb.js
@@ -11,6 +11,12 @@ export const AppBanner = (name) => `
                     Add "${name?? 'this source'}" to your Iphone.
                 </p>
             </div>
+            <div class="text-container">
+                <p class="title-text">KhoIPA Mod <span class="small beta badge"></span></p>
+                <p class="detail-text">
+                    Use "${name?? 'this source'}" as an application on your Iphone.
+                </p>
+            </div>
         </div>
         <a href="mobile.config">
             <button id="add-to-altstore">Add</button>
