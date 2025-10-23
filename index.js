dbd31d383121acf535600ac77b30c533aa958da8
drphe <mmail.phe@gmail.com>
2025-10-23 23:27:45 +0700

    update code

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ import UIAlert from "./common/vendor/uialert.js/uialert.js";
 const sources = await json("./common/assets/json/sources.json");
 const editorsources = await json("./common/assets/json/editorsources.json");
 
-(async () =>{
+(async () => {
     document.getElementById("top")?.insertAdjacentHTML("afterbegin", AppBanner("Kho IPA Mod"));
     // fetch Data
     const fetchedSources = await Promise.all(sources.map(async url => {
-      const source = await fetchSource(url);
-      return source || null;
+        const source = await fetchSource(url);
+        return source || null;
     }));
     const fetchedEditorSources = await Promise.all(editorsources.map(async url => {
-      const source = await fetchSource(url);
-      return source || null;
+        const source = await fetchSource(url);
+        return source || null;
     }));
 
     // Sort sources by last updated
@@ -32,21 +32,21 @@ const editorsources = await json("./common/assets/json/editorsources.json");
         await insertSource(source);
     }
 
-    const allSources = [...fetchedEditorSources, ...fetchedSources]; 
+    const allSources = [...fetchedEditorSources, ...fetchedSources];
     const allApps = [];
     for (const source of allSources) {
-      if (!source || !Array.isArray(source.apps)) continue;
-      for (const app of source.apps) {
-        app.sourceURL = source.sourceURL;
-      }
-      const nonBetaApps = source.apps.filter(app => !app.beta);
-      allApps.push(...nonBetaApps);
+        if (!source || !Array.isArray(source.apps)) continue;
+        for (const app of source.apps) {
+            app.sourceURL = source.sourceURL;
+        }
+        const nonBetaApps = source.apps.filter(app => !app.beta);
+        allApps.push(...nonBetaApps);
     }
     // sort app 
     allApps.sort((a, b) => {
-      const dateA = new Date(a.versionDate ?? a.versions?.[0]?.date ?? 0).valueOf();
-      const dateB = new Date(b.versionDate ?? b.versions?.[0]?.date ?? 0).valueOf();
-      return dateB - dateA;
+        const dateA = new Date(a.versionDate ?? a.versions?.[0]?.date ?? 0).valueOf();
+        const dateB = new Date(b.versionDate ?? b.versions?.[0]?.date ?? 0).valueOf();
+        return dateB - dateA;
     });
 
     document.body.classList.remove("loading");
@@ -63,66 +63,82 @@ const editorsources = await json("./common/assets/json/editorsources.json");
 
     // click button
     document.getElementById('search').addEventListener("click", (e) => {
-      const suggestions = document.getElementById('suggestions');
-      const repositories = document.getElementById('repositories');
-      const apps = document.getElementById('apps');
-      if (e.target.innerText == "View All Apps") {
-        suggestions.style.display = 'none';
-        repositories.style.display = 'none';
-        apps.style.display = 'block';
-        e.target.innerText = "Close";
-      } else {
-        suggestions.style.display = 'block';
-        repositories.style.display = 'block';
-        apps.style.display = 'none';
-        e.target.innerText = "View All Apps";
-      }
+        const suggestions = document.getElementById('suggestions');
+        const repositories = document.getElementById('repositories');
+        const apps = document.getElementById('apps');
+        if (e.target.innerText == "View All Apps") {
+            suggestions.style.display = 'none';
+            repositories.style.display = 'none';
+            apps.style.display = 'block';
+            e.target.innerText = "Close";
+        } else {
+            suggestions.style.display = 'block';
+            repositories.style.display = 'block';
+            apps.style.display = 'none';
+            e.target.innerText = "View All Apps";
+        }
     });
+
     async function run() {
-      appsContainer.innerHTML = "";
-      appsContainer.classList.add("skeleton-text", "skeleton-effect-wave");
-      const tasks = [];
-      for (let i = 0; i < 5; i++) {
-        tasks.push(AppLoading());
-      }
-      await Promise.all(tasks); // Chờ tất cả hoàn tất
+        appsContainer.innerHTML = "";
+        appsContainer.classList.add("skeleton-text", "skeleton-effect-wave");
+        const tasks = [];
+        for (let i = 0; i < 5; i++) {
+            tasks.push(AppLoading());
+        }
+        await Promise.all(tasks); // Chờ tất cả hoàn tất
     }
+
     // search box
     const searchBox = document.getElementById("filterText");
     searchBox.addEventListener("keydown", async (event) => {
-  if (event.key === "Enter") {
-    await run();
-    const keyword = searchBox.value.toLowerCase();
-    filteredApps = allApps.filter(app => app.name?.toLowerCase().includes(keyword));
-    if (filteredApps.length === 0) {
-      filteredApps = [...allApps];
-    }
+        if (event.key === "Enter") {
+            await run();
+            const keyword = searchBox.value.toLowerCase();
+            filteredApps = allApps.filter(app => app.name?.toLowerCase().includes(keyword));
+            if (filteredApps.length === 0) {
+                filteredApps = [...allApps];
+            }
 
-      // Nếu có kết quả
-      currentIndex = 0;
-      setTimeout(() => {
-        appsContainer.innerHTML = "";
-      loadMoreApps();
-        appsContainer.classList.remove("skeleton-text", "skeleton-effect-wave");
-        loadMoreApps();
-      }, 500);
-  }
-});
+            // Nếu có kết quả
+            currentIndex = 0;
+            setTimeout(() => {
+                appsContainer.innerHTML = "";
+                loadMoreApps();
+                appsContainer.classList.remove("skeleton-text", "skeleton-effect-wave");
+                loadMoreApps();
+            }, 500);
+        }
+    });
 
     function loadMoreApps() {
-      const nextApps = filteredApps.slice(currentIndex, currentIndex + appsPerLoad);
-      nextApps.forEach(app => {
-        appsContainer.insertAdjacentHTML("beforeend", `<div class="app-container">${AppHeader(app)}</div>`);
-      });
-      currentIndex += appsPerLoad;
+        const nextApps = filteredApps.slice(currentIndex, currentIndex + appsPerLoad);
+        nextApps.forEach(app => {
+            appsContainer.insertAdjacentHTML("beforeend", `<div class="app-container">${AppHeader(app)}</div>`);
+        });
+        currentIndex += appsPerLoad;
     }
+
+
+    appsContainer.addEventListener("click", event => {
+        const target = event.target.closest("a.app-header-link");
+        if (!target) return;
+
+        event.preventDefault();
+        const bundleId = target.getAttribute("bundleid-data");
+        const sourceTarget = allSources.find(json =>
+            json.apps.some(app => app.bundleIdentifier === bundleId)
+        );
+        if (!sourceTarget) return;
+        openPanel(sourceTarget, bundleId);
+    });
 
     loadMoreApps();
 
     async function fetchSource(url) {
         const data = await json(url);
-	const source = consolidateApps(data);
-	source.sourceURL = url
+        const source = consolidateApps(data);
+        source.sourceURL = url
         if (!source) return;
         source.lastUpdated = new Date("1970-01-01");
         source.appCount = 0;
@@ -142,9 +158,9 @@ const editorsources = await json("./common/assets/json/editorsources.json");
             source.iconURL = "./common/assets/img/generic_app.jpeg";
         if (!source.tintColor)
             source.tintColor = "var(--tint-color);";
-            source.url = url;
+        source.url = url;
         return source;
-        
+
     }
 
     async function insertSource(source, id = "repositories", position = "beforeend", flag = false) {
@@ -174,21 +190,7 @@ const editorsources = await json("./common/assets/json/editorsources.json");
     document.querySelectorAll("a.install").forEach(button => {
         button.addEventListener("click", event => {
             event.preventDefault();
-            showUIAlert("How To Install?","Select Share Button -> Add To Home Screen  -> Done");
-        });
-    });
-
-    // open app panel
-    document.querySelectorAll("a.app-header-link").forEach(button => {
-        button.addEventListener("click", event => {
-            event.preventDefault();
-	    const bundleId = event.currentTarget.getAttribute('bundleid-data');
-	    const sourceTarget = allSources.find(json => {
-		return json.apps.some(app => app.bundleIdentifier === bundleId);
-	    });
-	    console.log(bundleId,sourceTarget)
-	    if(!sourceTarget) return;
-            openPanel(sourceTarget, bundleId);
+            showUIAlert("How To Install?", "Select Share Button -> Add To Home Screen  -> Done");
         });
     });
 
@@ -196,11 +198,11 @@ const editorsources = await json("./common/assets/json/editorsources.json");
         const title = document.querySelector("h1");
         const navBar = document.getElementById("nav-bar");
         const navBarTitle = navBar.querySelector("#title");
-	const showItem = title.getBoundingClientRect().y < 36;
+        const showItem = title.getBoundingClientRect().y < 36;
 
-  	navBar.classList.toggle("hide-border", !showItem);
-  	navBarTitle.classList.toggle("hidden", !showItem);
+        navBar.classList.toggle("hide-border", !showItem);
+        navBarTitle.classList.toggle("hidden", !showItem);
 
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) loadMoreApps();
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) loadMoreApps();
     }
 })();
--- view/view.js
+++ view/view.js
@@ -10,7 +10,7 @@ const editorsources = await json("../common/assets/json/editorsources.json");
 main(json => {
     document.getElementById("edit").addEventListener("click", e => {
         e.preventDefault();
-	if(editorsources.includes(sourceURL)) open(`../repo.html?source=${sourceURL}`);
+        if (editorsources.includes(sourceURL)) open(`../repo.html?source=${sourceURL}`);
     });
 
     document.getElementById("add")?.addEventListener("click", e => {
@@ -39,14 +39,15 @@ main(json => {
         if (json.news.length == 1) {
             document.getElementById("news-items").insertAdjacentHTML("beforeend", NewsItem(json.news[0], true));
             document.getElementById("news-items").classList.add("one");
-        } else for (let i = 0; i < 5 && i < json.news.length; i++)
-            document.getElementById("news-items").insertAdjacentHTML("beforeend", NewsItem(json.news[i], true));
+        } else
+            for (let i = 0; i < 5 && i < json.news.length; i++)
+                document.getElementById("news-items").insertAdjacentHTML("beforeend", NewsItem(json.news[i], true));
     } else document.getElementById("news").remove();
 
     json.apps.sort((a, b) => {
-      const dateA = new Date(a.versionDate ?? a.versions?.[0]?.date ?? 0).valueOf();
-      const dateB = new Date(b.versionDate ?? b.versions?.[0]?.date ?? 0).valueOf();
-      return dateB - dateA;
+        const dateA = new Date(a.versionDate ?? a.versions?.[0]?.date ?? 0).valueOf();
+        const dateB = new Date(b.versionDate ?? b.versions?.[0]?.date ?? 0).valueOf();
+        return dateB - dateA;
     });
 
     // 
@@ -67,82 +68,85 @@ main(json => {
         });
     }
 
-  const allApps = json.apps.filter(app => !app.beta);
-  let filteredApps = [...allApps];
-  let currentIndex = 0;
-  const appsPerLoad = 4;
-      const appsTitle = document.getElementById('apps-title');
-      const appsContainer = document.getElementById('apps-list');
-      const featured = document.getElementById('featured');
+    const allApps = json.apps.filter(app => !app.beta);
+    let filteredApps = [...allApps];
+    let currentIndex = 0;
+    const appsPerLoad = 4;
+    const appsTitle = document.getElementById('apps-title');
+    const appsContainer = document.getElementById('apps-list');
+    const featured = document.getElementById('featured');
 
-  // Tạo wrapper chứa input và icon
-  const searchWrapper = document.createElement("div");
-  searchWrapper.style.cssText = "display: none;z-index: 200;align-items: center;justify-content: center;gap: 0.85rem;position: sticky;top: 6.5rem;margin-bottom: 1rem;margin: 10px;"
+    // Tạo wrapper chứa input và icon
+    const searchWrapper = document.createElement("div");
+    searchWrapper.style.cssText = "display: none;z-index: 200;align-items: center;justify-content: center;gap: 0.85rem;position: sticky;top: 6.5rem;margin-bottom: 1rem;margin: 10px;"
 
-  // Tạo ô tìm kiếm
-  const searchBox = document.createElement("input");
-  searchBox.type = "text";
-  searchBox.placeholder = "Enter app name...";
-  searchBox.className = "form-control mb-3";
-  searchBox.style.cssText = "width: 100%; padding-right: 35px; box-sizing: border-box; border-radius: 20px; "
+    // Tạo ô tìm kiếm
+    const searchBox = document.createElement("input");
+    searchBox.type = "text";
+    searchBox.placeholder = "Enter app name...";
+    searchBox.className = "form-control mb-3";
+    searchBox.style.cssText = "width: 100%; padding-right: 35px; box-sizing: border-box; border-radius: 20px; "
 
-  // Tạo icon kính lúp
-  const searchIcon = document.createElement("span");
-  searchIcon.innerHTML = ` <i class="bi bi-search"></i>`
-  searchIcon.style.cssText="position: absolute;right: 1rem;top: 60%;transform: translateY(-50%);pointer-events: none;color: rgb(136, 136, 136);";
+    // Tạo icon kính lúp
+    const searchIcon = document.createElement("span");
+    searchIcon.innerHTML = ` <i class="bi bi-search"></i>`
+    searchIcon.style.cssText = "position: absolute;right: 1rem;top: 60%;transform: translateY(-50%);pointer-events: none;color: rgb(136, 136, 136);";
 
-  // Gắn các phần tử
-  searchWrapper.appendChild(searchBox);
-  searchWrapper.appendChild(searchIcon);
-  appsContainer.before(searchWrapper);
+    // Gắn các phần tử
+    searchWrapper.appendChild(searchBox);
+    searchWrapper.appendChild(searchIcon);
+    appsContainer.before(searchWrapper);
 
-    searchBox.addEventListener("keydown", async(event) => {
-  if (event.key === "Enter") {
-    await run();
-    const keyword = searchBox.value.toLowerCase();
-    filteredApps = allApps.filter(app => app.name?.toLowerCase().includes(keyword));
-    if (filteredApps.length === 0) {
-      filteredApps = [...allApps];
-    }
+    searchBox.addEventListener("keydown", async (event) => {
+        if (event.key === "Enter") {
+            await run();
+            const keyword = searchBox.value.toLowerCase();
+            filteredApps = allApps.filter(app => app.name?.toLowerCase().includes(keyword));
+            if (filteredApps.length === 0) {
+                filteredApps = [...allApps];
+            }
 
-      // Nếu có kết quả
-      currentIndex = 0;
-      setTimeout(() => {
-        appsContainer.innerHTML = "";
-      loadMoreApps();
-        appsContainer.classList.remove("skeleton-text", "skeleton-effect-wave");
-        loadMoreApps();
-      }, 500);
-  }
+            // Nếu có kết quả
+            currentIndex = 0;
+            setTimeout(() => {
+                appsContainer.innerHTML = "";
+                loadMoreApps();
+                appsContainer.classList.remove("skeleton-text", "skeleton-effect-wave");
+                loadMoreApps();
+            }, 500);
+        }
 
     });
 
-    async function run() {
-      appsContainer.innerHTML = "";
-      appsContainer.classList.add("skeleton-text", "skeleton-effect-wave");
-      const tasks = [];
-      for (let i = 0; i < 5; i++) {
-        tasks.push(AppLoading());
-      }
-      await Promise.all(tasks); // Chờ tất cả hoàn tất
-    }
     // click button
     document.getElementById('search').addEventListener("click", (e) => {
-      e.preventDefault();
-      if (e.target.innerText == "View All Apps") {
-        appsContainer.style.display = 'block';
-        featured.style.display = 'none';
-        appsTitle.innerHTML = `${allApps.length} Apps`;
-        e.target.innerText = "Close";
-	searchWrapper.style.display = "block";
-      } else {
-	searchWrapper.style.display = "none";
-        featured.style.display = 'block';
-        appsContainer.style.display = 'none';
-        appsTitle.innerHTML = "Featured Apps";
-        e.target.innerText = "View All Apps";
-      }
+        e.preventDefault();
+        if (e.target.innerText == "View All Apps") {
+            appsContainer.style.display = 'block';
+            featured.style.display = 'none';
+            appsTitle.innerHTML = `${allApps.length} Apps`;
+            e.target.innerText = "Close";
+            searchWrapper.style.display = "block";
+        } else {
+            searchWrapper.style.display = "none";
+            featured.style.display = 'block';
+            appsContainer.style.display = 'none';
+            appsTitle.innerHTML = "Featured Apps";
+            e.target.innerText = "View All Apps";
+        }
     });
+
+
+    async function run() {
+        appsContainer.innerHTML = "";
+        appsContainer.classList.add("skeleton-text", "skeleton-effect-wave");
+        const tasks = [];
+        for (let i = 0; i < 5; i++) {
+            tasks.push(AppLoading());
+        }
+        await Promise.all(tasks); // Chờ tất cả hoàn tất
+    }
+
     //with screenshot
     function loadMoreApps() {
         const nextApps = filteredApps.slice(currentIndex, currentIndex + appsPerLoad);
@@ -175,6 +179,16 @@ main(json => {
         });
         currentIndex += appsPerLoad;
     }
+
+    appsContainer.addEventListener("click", event => {
+        const target = event.target.closest("a.app-header-link");
+        if (!target) return;
+
+        event.preventDefault();
+        const bundleId = target.getAttribute("bundleid-data");
+        openPanel(json, bundleId, '..');
+    });
+
     loadMoreApps();
 
     // 
@@ -192,24 +206,15 @@ main(json => {
     `);
     if (!description && !json.website) document.getElementById("about").remove();
 
-    // open app panel
-    document.querySelectorAll("a.app-header-link").forEach(button => {
-        button.addEventListener("click", event => {
-            event.preventDefault();
-	    const bundleId = event.currentTarget.getAttribute('bundleid-data');
-            openPanel(json, bundleId, '..');
-        });
-    });
-
     window.onscroll = e => {
         const title = document.querySelector("h1");
         const navBar = document.getElementById("nav-bar");
         const navBarTitle = navBar.querySelector("#title");
-	const showItem = title.getBoundingClientRect().y < 85;
+        const showItem = title.getBoundingClientRect().y < 85;
 
-  	navBar.classList.toggle("hide-border", !showItem);
-  	navBarTitle.classList.toggle("hidden", !showItem);
+        navBar.classList.toggle("hide-border", !showItem);
+        navBarTitle.classList.toggle("hidden", !showItem);
 
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) loadMoreApps();
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) loadMoreApps();
     }
 }, "../");
