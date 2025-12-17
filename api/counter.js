01a9b93cf8e90c4277c104e842d47db5dd078a82
drphe <mmail.phe@gmail.com>
2025-12-17 17:53:14 +0700

    Update counter.js

--- api/counter.js
+++ api/counter.js
@@ -1,10 +1,10 @@
-// api/counter.js
-let count = 0;
+import { kv } from '@vercel/kv';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'GET') {
-    count++;
-    res.status(200).json({ views: count });
+    // tăng bộ đếm
+    const views = await kv.incr('page_views');
+    res.status(200).json({ views });
   } else {
     res.status(405).json({ error: 'Method not allowed' });
   }
