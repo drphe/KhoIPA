9ee22743e20989d3a1e9aa3311ade72fc0fdcffc
drphe <138484118+drphe@users.noreply.github.com>
2026-01-08 15:08:23 +0700

    Update noti.js

--- noti.js
+++ noti.js
@@ -11,12 +11,11 @@ self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SHOW_GREETING') {
         const options = {
             body: event.data.body,
-            icon: 'https://via.placeholder.com/192',
-            badge: 'https://via.placeholder.com/192',
+            icon: 'icon/logo.png',
+            badge: 'icon/logo.png',
             vibrate: [100, 50, 100],
             data: { dateOfArrival: Date.now() }
         };
-
         self.registration.showNotification(event.data.title, options);
     }
 });
